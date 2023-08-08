df2025568ded89d9fe5f0e977d042c9ff7c64ae7
Robin Pindel <robin.pindel@gmail.com>
2023-08-08 07:42:24 -0500

    mod1 simple php file assignment final code

---
+++ mod1/PindelFirstProgram.php
@@ -0,0 +1,29 @@
+<!--
+Robin Pindel
+440 mod1 Assignment
+8/8/2023
+-->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      Pindel 440 mod1 Simple PHP File
+    </title>
+  </head>
+
+  <body>
+
+    <?php
+
+    $welcome = "Welcome";
+    $to = "to";
+    $php = "PHP";
+    $class = "class";
+    
+    echo "<h1>" . $welcome . " " . $to . " " . $php . " " . $class . "!</h1>";
+
+    ?>
+
+  </body>
+</html>
