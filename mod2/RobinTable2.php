be02b7f014948c1574482719de53c0e79de9a54e
Robin Pindel <robin.pindel@gmail.com>
2023-08-17 07:53:40 -0500

    mod2 final code

--- mod2/RobinTable2.php
+++ mod2/RobinTable2.php
@@ -1,11 +1,11 @@
 <!--
 Robin Pindel
-440 mod1 Assignment
+440 mod2 Assignment
 8/17/2023
 
-HTML table via nestedt PHP loops mod2 programming assignment.  Code generates
+HTML table via nested PHP loops mod2 programming assignment.  Code generates
 a random number of rows and columns for the table between 1 and 10 and then 
-inserts a random integer from 1 to 100 into each cell.
+inserts a random integer from 1 to 100 into each table cell.
 -->
 
 <!DOCTYPE html>
@@ -16,45 +16,51 @@ inserts a random integer from 1 to 100 into each cell.
 </head>
 <body>
 
-<div id="table-container">
+<div id="page-container">
+  <div id="label-container">
+    <h1>Random Integers 1 - 100</h1>
+  </div>
 
-<?PHP 
-$rows = rand(1, 10);
-$cols = rand(1, 10);
-?>
+  <div id="table-container">
 
-<table>
-  <caption>Random Integers 1 - 100</caption>
-<?PHP
-for ($i = 1; $i <= $rows; ++$i) {
+  <?PHP 
+  $rows = rand(1, 10); #Generate random number of rows for table
+  $cols = rand(1, 10); #Generate random number of columns for table
   ?>
-  
-  <tr>
+
+  <table>
   
   <?PHP
-  for ($j = 1; $j <= $cols; ++$j) {
+  for ($i = 1; $i <= $rows; ++$i) {
     ?>
+  
+    <tr>
+  
+    <?PHP
+    for ($j = 1; $j <= $cols; ++$j) {
+      ?>
 
-    <td>
+      <td>
     
-    <?PHP 
-    echo(rand(1,100));
+      <?PHP 
+      echo(rand(1, 100));
+      ?>
+
+      </td>
+
+      <?PHP
+    }
     ?>
 
-    </td>
+    </tr>
 
     <?PHP
   }
-  ?>
+  ?>  
 
-  </tr>
+  </table>
 
-  <?PHP
-}
-?>  
-
-</table>
-
+  </div>
 </div>
 
 </body>
