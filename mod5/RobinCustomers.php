3fd5968da883f4a09a19fa460d0f9d44ed55e1a3
Robin Pindel <robin.pindel@gmail.com>
2023-08-29 10:07:23 -0500

    more mod5 code tweaks, better

--- mod5/RobinCustomers.php
+++ mod5/RobinCustomers.php
@@ -14,71 +14,71 @@ PHP program displaying and working with customer array data.
 <?php
 
 $c1 = array(
-  "fname" => "Alpha", 
-  "lname" => "Smith", 
+  "firstname" => "Alpha", 
+  "lastname" => "Smith", 
   "age" => "37", 
   "phone" => "111-111-1111"
 );
 
 $c2 = array(
-  "fname" => "Beta", 
-  "lname" => "Jones", 
+  "firstname" => "Beta", 
+  "lastname" => "Jones", 
   "age" => "23", 
   "phone" => "222-222-2222"
 );
 
 $c3 = array(
-  "fname" => "Charlie", 
-  "lname" => "Smith", 
+  "firstname" => "Charlie", 
+  "lastname" => "Smith", 
   "age" => "25", 
   "phone" => "333-333-3333"
 );
 
 $c4 = array(
-  "fname" => "Delta", 
-  "lname" => "Jones", 
+  "firstname" => "Delta", 
+  "lastname" => "Jones", 
   "age" => "14", 
   "phone" => "444-444-4444"
 );
 
 $c5 = array(
-  "fname" => "Echo", 
-  "lname" => "Greyson", 
+  "firstname" => "Echo", 
+  "lastname" => "Greyson", 
   "age" => "17", 
   "phone" => "555-555-5555"
 );
 
 $c6 = array(
-  "fname" => "Foxtrot", 
-  "lname" => "Smith", 
+  "firstname" => "Foxtrot", 
+  "lastname" => "Smith", 
   "age" => "27", 
   "phone" => "666-666-6666"
 );
 
 $c7 = array(
-  "fname" => "Golf", 
-  "lname" => "Greyson", 
+  "firstname" => "Golf", 
+  "lastname" => "Greyson", 
   "age" => "46", 
   "phone" => "777-777-7777"
 );
 
 $c8 = array(
-  "fname" => "Hotel", 
-  "lname" => "Greyson", 
+  "firstname" => "Hotel", 
+  "lastname" => "Greyson", 
   "age" => "53", 
   "phone" => "888-888-8888"
 );
 
 $c9 = array(
-  "fname" => "India", 
-  "lname" => "Greyson", 
+  "firstname" => "India", 
+  "lastname" => "Greyson", 
   "age" => "19", 
   "phone" => "999-999-9999"
 );
 
 $c10 = array(
-  "fname" => "Juliett", 
-  "lname" => "Smith", 
+  "firstname" => "Juliett", 
+  "lastname" => "Smith", 
   "age" => "13", 
   "phone" => "202-202-2020"
 );
@@ -122,9 +122,47 @@ echo "</table>";
 
 <?PHP
 
+/*
 $key = array_column($customers, "age");
 array_multisort($key, SORT_DESC, $customers);
+*/
 
+$whoage = array_filter($customers,
+  function ($customer) {
+      return $customer['age'] === '37';
+  }
+);
+
+echo "<p>The customer details for those who are 37 years old:</p>";
+foreach($whoage as $who) {
+  foreach($who as $what => $details) {
+    echo "{$what} : {$details}";
+    echo "<br />";
+  }
+}
+
+####
+
+$whoname = array_filter($customers,
+  function ($customer) {
+      return $customer['lastname'] === 'Smith';
+  }
+);
+
+echo "<p>The customer details for those whose last name is Smith:</p>";
+foreach($whoname as $who) {
+  foreach($who as $what => $details) {
+    echo "{$what} : {$details}";
+    echo "<br />";
+  }
+  echo "<br />";
+}
+echo "<br />";
+
+####
+
+$age37 = $whoage[0]["firstname"];
+echo "$age37 is 37 years old.";
 
 ?>
 
